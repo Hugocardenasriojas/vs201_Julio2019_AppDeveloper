2b229336a21af9686eb2cdf3605d1334d9269348
Hugocardenasriojas <52942824+Hugocardenasriojas@users.noreply.github.com>
2019-07-24 21:50:54 -0500

    24/07/2019 02
    Segunda hora

---
+++ cap01/slnApp/App.Entities.Base/Invoice.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Entities.Base
+{
+    public class Invoice
+    {
+        public int InvoiceId { get; set; }
+        public int CustomerId { get; set; }
+        public DateTime InvoiceDate { get; set; }
+        public String BillingAddress { get; set; }
+        public String BillingCity { get; set; }
+        public String BillingState { get; set; }
+        public String BillingCountry { get; set; }
+        public String BillingPostalCode { get; set; }
+        public Decimal Total { get; set; }
+        /*
+            [BillingAddress], 
+            [BillingCity], [BillingState], 
+            [BillingCountry], [BillingPostalCode], 
+            [Total]
+        */
+        public List<InvoiceLine> InvoiceLine { get; set; }
+    }
+}
